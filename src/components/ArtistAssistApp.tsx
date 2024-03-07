d1df99ce64c5a1b5251de291fae7f3b36a648f81
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-03-07 12:36:54 +0200

    Improve error messages

--- src/components/ArtistAssistApp.tsx
+++ src/components/ArtistAssistApp.tsx
@@ -9,7 +9,7 @@ import {
   QuestionCircleOutlined,
 } from '@ant-design/icons';
 import type {TabsProps} from 'antd';
-import {Alert, App, Col, Flex, FloatButton, List, Row, Space, Tabs, Typography, theme} from 'antd';
+import {App, Col, FloatButton, Row, Space, Tabs, Typography, theme} from 'antd';
 import {useCallback, useContext, useEffect, useRef, useState} from 'react';
 import StickyBox from 'react-sticky-box';
 import {useEventListener, useTimeout} from 'usehooks-ts';
@@ -26,6 +26,7 @@ import {
 } from '../services/color';
 import {Rgb, RgbTuple} from '../services/color/model';
 import {IMAGE_SIZE, createScaledImageBitmap} from '../utils';
+import {BrowserNotSupported} from './BrowserNotSupported';
 import {Help} from './Help';
 import {ImageColorPicker} from './ImageColorPicker';
 import {ImageGrid} from './ImageGrid';
@@ -38,16 +39,13 @@ import {PaintSetSelect} from './PaintSetSelect';
 import {Palette} from './Palette';
 import {TabKey} from './types';
 
-const browserFeatures = {
+const browserFeatures: Record<string, boolean> = {
   Worker: typeof Worker !== 'undefined',
   OffscreenCanvas: typeof OffscreenCanvas !== 'undefined',
   createImageBitmap: typeof createImageBitmap !== 'undefined',
   indexedDB: typeof indexedDB !== 'undefined',
 };
 const isBrowserSupported = Object.values(browserFeatures).every(value => value);
-const browserFeaturesListItems = Object.entries(browserFeatures).map(
-  ([feature, isSupported]) => `${isSupported ? '✔️' : '❗'} ${feature}`
-);
 
 const {paintSet: importedPaintSet, paintMix: importedPaintMix}: UrlParsingResult = parseUrl(
   window.location.toString()
@@ -239,23 +237,7 @@ export const ArtistAssistApp: React.FC = () => {
   );
 
   if (!isBrowserSupported) {
-    return (
-      <Flex vertical gap="middle" style={{margin: 16}}>
-        <Alert
-          message="Error"
-          description={`Your web browser is not supported. Use the latest version of Google Chrome, Firefox, Opera, Samsung Internet or Microsoft Edge to access the web app.`}
-          type="error"
-          showIcon
-        />
-        <List
-          size="small"
-          bordered
-          header={navigator.userAgent}
-          dataSource={browserFeaturesListItems}
-          renderItem={item => <List.Item>{item}</List.Item>}
-        />
-      </Flex>
-    );
+    return <BrowserNotSupported browserFeatures={browserFeatures} />;
   }
 
   return (
---
+++ src/components/BrowserNotSupported.tsx
@@ -0,0 +1,98 @@
+/**
+ * Copyright 2023 Eugene Khyst
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import {
+  AndroidOutlined,
+  AppleOutlined,
+  ChromeOutlined,
+  DesktopOutlined,
+  LinuxOutlined,
+  MobileOutlined,
+  TabletOutlined,
+  WindowsOutlined,
+} from '@ant-design/icons';
+import {Alert, Typography} from 'antd';
+import {UAParser} from 'ua-parser-js';
+
+const {browser, os, ua: userAgent}: UAParser.IResult = UAParser(navigator.userAgent);
+const prettyUserAgent = [browser.name, browser.version, os.name && 'on', os.name, os.version]
+  .filter(value => !!value)
+  .join(' ');
+
+type Props = {
+  browserFeatures: Record<string, boolean>;
+};
+
+export const BrowserNotSupported: React.FC<Props> = ({browserFeatures}: Props) => {
+  const browserFeaturesListItems = Object.entries(browserFeatures).map(
+    ([feature, isSupported]) => `${feature} ${isSupported ? '✔️' : '❗'}`
+  );
+
+  return (
+    <div style={{padding: 16}}>
+      <Alert
+        message={`Your web browser ${prettyUserAgent} is not supported.`}
+        type="error"
+        showIcon
+      />
+      <Typography.Title level={2}>Supported web browsers and operating systems</Typography.Title>
+      <Typography.Paragraph>
+        <ul>
+          <li>
+            <TabletOutlined /> Tablet and <MobileOutlined /> Mobile
+            <li>
+              <AppleOutlined /> iPadOS 16.4+ or iOS 16.4+ (not supported in any browser on older
+              versions of iPadOS and iOS)
+            </li>
+            <li>
+              <AndroidOutlined /> Android
+              <ul>
+                <li>Chrome 69+</li>
+                <li>Firefox 105+</li>
+                <li>Opera 73+</li>
+                <li>Samsung Internet 10.1+</li>
+              </ul>
+            </li>
+          </li>
+          <li>
+            <DesktopOutlined /> Desktop
+            <ul>
+              <li>
+                <AppleOutlined /> macOS
+                <ul>
+                  <li>
+                    Safari 16.4 (macOS Big Sur, macOS Monterey, macOS Ventura or later required)
+                  </li>
+                  <li>or install the latest version of Chrome, Edge, Firefox or Opera</li>
+                </ul>
+              </li>
+              <li>
+                <WindowsOutlined /> Windows and <LinuxOutlined /> Linux
+                <ul>
+                  <li>Chrome 69+</li>
+                  <li>Edge 79+</li>
+                  <li>Firefox 105+</li>
+                  <li>Opera 64+</li>
+                </ul>
+              </li>
+              <li>
+                <ChromeOutlined /> ChromeOS 69+
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </Typography.Paragraph>
+      <Typography.Title level={2}>Diagnostics</Typography.Title>
+      <Typography.Paragraph>{userAgent}</Typography.Paragraph>
+      <Typography.Paragraph>
+        <ul>
+          {browserFeaturesListItems.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </Typography.Paragraph>
+    </div>
+  );
+};
