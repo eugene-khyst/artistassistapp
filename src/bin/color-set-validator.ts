ad1a274db26dab3d653f5fbbb1aa9db42a7597ff
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-04-21 21:36:02 +0300

    Fix color sets

--- src/bin/color-set-validator.ts
+++ src/bin/color-set-validator.ts
@@ -4,20 +4,41 @@
  */
 
 import {readFileSync, readdirSync, statSync} from 'fs';
-import {StoreBoughtPaintSet} from '~/src/services/color';
+import {join} from 'path';
+import {PaintRecord, StoreBoughtPaintSet} from '~/src/services/color';
 
-function findDuplicates(arr: number[]): number[] {
-  return arr.filter((item, index) => arr.indexOf(item) !== index);
+function findDuplicates(array: number[]): number[] {
+  return array.filter((element, index) => array.indexOf(element) !== index);
 }
 
-function processFile(filePath: string) {
-  console.log('Processing file', filePath);
-  const data: string = readFileSync(filePath, 'utf8');
-  const paintSets: StoreBoughtPaintSet[] = JSON.parse(data);
-  for (const {name, colors} of paintSets) {
+function findDifference(array1: number[], array2: number[]): number[] {
+  return array1.filter(element => !array2.includes(element));
+}
+
+function processFiles(dirPath: string) {
+  console.log('Processing files in ', dirPath);
+  const setsPath: string = join(dirPath, 'sets.json');
+  const setsStr: string = readFileSync(setsPath, 'utf8');
+  const colorSets: StoreBoughtPaintSet[] = JSON.parse(setsStr);
+  const colorPath: string = join(dirPath, 'colors.json');
+  const colorsStr: string = readFileSync(colorPath, 'utf-8');
+  const colorDefs: PaintRecord[] = JSON.parse(colorsStr);
+  for (const [id, name, hex, rho] of colorDefs) {
+    if (!hex || !rho?.length) {
+      throw new Error(`Color ${name} (${id}) in ${colorPath} is missing RGB or reflectance`);
+    }
+  }
+  const colorIds: number[] = colorDefs.map(([id]: PaintRecord) => id);
+  for (const {name, colors} of colorSets) {
     const duplicates = findDuplicates(colors);
     if (duplicates.length > 0) {
-      throw new Error(`${name} in ${filePath} contains duplicates: ${duplicates.join(',')}`);
+      throw new Error(`Set ${name} in ${setsPath} contains duplicates: ${duplicates.join(',')}`);
+    }
+    const difference = findDifference(colors, colorIds);
+    if (difference.length > 0) {
+      throw new Error(
+        `Set ${name} in ${setsPath} contains missing colors: ${difference.join(',')}`
+      );
     }
   }
 }
@@ -31,7 +52,7 @@ function processDir(dirPath: string) {
     if (statSync(filePath).isDirectory()) {
       processDir(filePath);
     } else if (file === 'sets.json') {
-      processFile(filePath);
+      processFiles(dirPath);
     }
   });
 }
