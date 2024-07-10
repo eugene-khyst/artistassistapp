02dc750864f49be4a2d96c223ca965afa1c0f2df
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-07-10 13:59:12 +0300

    Progressive Web App (PWA)

--- src/file-handler.ts
+++ src/file-handler.ts
@@ -3,14 +3,14 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {saveImageFile} from '~/src/services/db';
+import {useAppStore} from '~/src/stores/app-store';
 
 export function registerFileHandler() {
   if ('launchQueue' in window) {
     window.launchQueue.setConsumer(async launchParams => {
       for (const fileHandle of launchParams.files) {
         const file: File = await fileHandle.getFile();
-        await saveImageFile({
+        await useAppStore.getState().saveRecentImageFile({
           file,
           date: new Date(),
         });
