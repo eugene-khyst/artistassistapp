012e468db7db6625af054cc43092bfa3499ef025
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-07-12 11:46:52 +0300

    Progressive Web App (PWA)

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -8,12 +8,8 @@ import {commitHash} from '~/src/config';
 export async function fetchAndCache(request: string | URL | Request): Promise<Response> {
   const response: Response = await fetch(request);
   const cache: Cache = await caches.open(commitHash);
-  const cacheResponse: Response | undefined = await caches.match(request);
-  if (cacheResponse?.headers.get('ETag') !== response.headers.get('ETag')) {
-    void cache.put(request, response.clone());
-    console.log('Caching', request);
-    console.log('Old ETag', cacheResponse?.headers.get('ETag'));
-    console.log('New ETag', response.headers.get('ETag'));
-  }
+  void cache.put(request, response.clone());
+  console.log('Caching', request);
+  console.log('Headers', response.headers);
   return response;
 }
