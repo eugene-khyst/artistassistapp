0f6a187af5785cfa9c032feb7a4342b681c9a769
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-07-12 16:03:29 +0300

    Progressive Web App (PWA)

--- src/root.tsx
+++ src/root.tsx
@@ -19,13 +19,6 @@ registerServiceWorker();
 registerFileHandler();
 void initAppStore();
 
-addEventListener('online', event => {
-  alert(navigator.onLine);
-});
-addEventListener('offline', event => {
-  alert(navigator.onLine);
-});
-
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
