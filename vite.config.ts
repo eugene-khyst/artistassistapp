0eefb71ccdf0623adc84d67f79864ca7e1dba151
Eugene Khyst <evgeniy.khyst@gmail.com>
2024-11-30 21:19:22 +0200

    Add Sentry Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import {sentryVitePlugin} from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import {defineConfig} from 'vite';
 import {VitePWA} from 'vite-plugin-pwa';
@@ -12,14 +13,24 @@ export default defineConfig({
       manifest: false,
       injectRegister: false,
     }),
+    sentryVitePlugin({
+      org: 'eugene-khyst',
+      project: 'artistassistapp',
+    }),
   ],
+
   resolve: {
     alias: [{find: /^~\/src\//, replacement: '/src/'}],
   },
+
   server: {
     headers: {
       'Cross-Origin-Opener-Policy': 'same-origin',
       'Cross-Origin-Embedder-Policy': 'require-corp',
     },
+  },
+
+  build: {
+    sourcemap: true,
   },
 });
